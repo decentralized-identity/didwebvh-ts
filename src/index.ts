70ccb7858f017f4f37cba9b359da03f9f07ee3b3
Brian Richter <brian@aviary.tech>
2026-06-24 23:36:07 -0700

    feat: export getResolver, defaultVerifier, and result types
    Signed-off-by: Brian Richter <brian@aviary.tech>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export { AbstractCrypto, createDocumentSigner, createProof, createSigner, prepareDataForSigning } from './cryptography';
 export * from './interfaces';
 export { createDID, deactivateDID, resolveDID, resolveDIDFromLog, updateDID } from './method';
+export { getResolver } from './resolver';
+export type { GetResolverConfig } from './resolver';
+export { defaultVerifier } from './verifier';
+export { InvalidDidUrlError } from './resolver-result';
+export type { WebvhDocumentMetadata, WebvhErrorCode, WebvhResolutionMetadata } from './resolver-result';
 export { deriveNextKeyHash, generateParallelDidWeb, parseDidKeyDid, parseDidKeyVerificationMethod } from './utils';
 export { MultibaseEncoding, multibaseDecode, multibaseEncode } from './utils/multiformats';
 export {
